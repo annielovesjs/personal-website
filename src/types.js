e9f5485a0733721a01f1eae9b4461135ed20f3d0
Annie Yuan Zhi Liang <aliang27@uwo.ca>
2020-12-28 02:28:36 -0500

    new proj - spade

--- src/types.js
+++ src/types.js
@@ -1,64 +1,72 @@
 export const INTRO = "Hi, I'm Annie.";
 export const SOCIAL = "Let's chat!";
-export const INTRO_TYPE_TEXT = ["I help create fun experiences", "I travel the world in search of good food"];
-export const PROJECT_OBJ = [
-    {
-        'name': 'The Food Hunt',
-        'tools': ['React', 'D3'],
-        'link': 'https://foodhunt.annieliang.io/',
-        'img': './pics/foodhunt.jpg',
-        'live': true
-    },
-    {
-        'name': 'Web Template Studio',
-        'tools': ['TypeScript', 'React', 'Flask', 'CosmosDB'],
-        'link': 'https://marketplace.visualstudio.com/items?itemName=WASTeamAccount.WebTemplateStudio-dev-nightly',
-        'img': './pics/wts.jpg',
-        'live': true
-    },
-    {
-        'name': 'Dice It Up',
-        'tools': ['HTML & CSS','JavaScript'],
-        'link': 'https://diceitup.annieliang.io/',
-        'img': './pics/diceitup.png',
-        'live': true
-    },
-    {
-        'name': 'Spice Up Your Style',
-        'tools': ['HTML & CSS', 'JavaScript'],
-        'link': 'https://github.com/annielovesjs/designsbytara',
-        'img': './pics/taraweaver.jpg',
-        'live': false
-    },
-    {
-        'name': 'This Just Got Real',
-        'tools': ['HTML & CSS', 'JavaScript'],
-        'link': 'https://github.com/annielovesjs/bendmello-client-site',
-        'img': './pics/bendmello.png',
-        'live': false
-    },
-    {
-        'name': 'Can You Beat An AI?',
-        'tools': ['HTML & CSS', 'React'],
-        'link': 'https://github.com/annielovesjs/kinect-4',
-        'img': './pics/Kinect4.png',
-        'live': false
-    },
-    {
-        'name': 'Plattr',
-        'tools': ['React Native'],
-        'link': 'https://github.com/annielovesjs/plattr',
-        'img': './pics/plattr.jpg',
-        'live': false
-    },
-    {
-        'name': 'Guard Me',
-        'tools': ['HTML & CSS'],
-        'link': 'https://www.youtube.com/watch?v=tW5TmxMhtCU',
-        'img': './pics/angels.png',
-        'live': false
-    }
+export const INTRO_TYPE_TEXT = [
+	"I help create fun experiences",
+	"I travel the world in search of good food",
 ];
-
-
-
+export const PROJECT_OBJ = [
+	{
+		name: "Spade",
+		tools: ["Socket.io", "Node.js", "Javascript"],
+		link: "http://ec2-3-96-195-143.ca-central-1.compute.amazonaws.com:3000/",
+		img: "./pics/spade.png",
+		live: true,
+	},
+	{
+		name: "The Food Hunt",
+		tools: ["React", "D3"],
+		link: "https://foodhunt.annieliang.io/",
+		img: "./pics/foodhunt.jpg",
+		live: true,
+	},
+	{
+		name: "Web Template Studio",
+		tools: ["TypeScript", "React", "Flask", "CosmosDB"],
+		link:
+			"https://marketplace.visualstudio.com/items?itemName=WASTeamAccount.WebTemplateStudio-dev-nightly",
+		img: "./pics/wts.jpg",
+		live: true,
+	},
+	{
+		name: "Dice It Up",
+		tools: ["HTML & CSS", "JavaScript"],
+		link: "https://diceitup.annieliang.io/",
+		img: "./pics/diceitup.png",
+		live: true,
+	},
+	{
+		name: "Spice Up Your Style",
+		tools: ["HTML & CSS", "JavaScript"],
+		link: "https://github.com/annielovesjs/designsbytara",
+		img: "./pics/taraweaver.jpg",
+		live: false,
+	},
+	{
+		name: "This Just Got Real",
+		tools: ["HTML & CSS", "JavaScript"],
+		link: "https://github.com/annielovesjs/bendmello-client-site",
+		img: "./pics/bendmello.png",
+		live: false,
+	},
+	{
+		name: "Can You Beat An AI?",
+		tools: ["HTML & CSS", "React"],
+		link: "https://github.com/annielovesjs/kinect-4",
+		img: "./pics/Kinect4.png",
+		live: false,
+	},
+	{
+		name: "Plattr",
+		tools: ["React Native"],
+		link: "https://github.com/annielovesjs/plattr",
+		img: "./pics/plattr.jpg",
+		live: false,
+	},
+	{
+		name: "Guard Me",
+		tools: ["HTML & CSS"],
+		link: "https://www.youtube.com/watch?v=tW5TmxMhtCU",
+		img: "./pics/angels.png",
+		live: false,
+	},
+];
